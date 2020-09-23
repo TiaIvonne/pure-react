a0edaf50b0078e32f756d69a6f0a96ed93c0b987
Ivonne M <ivonne.wftm@gmail.com>
2020-09-23 14:41:22 +0200

    23-09

--- Exercises cap 11/01/src/index.js
+++ Exercises cap 11/01/src/index.js
@@ -3,43 +3,73 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 
-// inicializo clase House
+// funcion texto, esta funcion cambia el color del texto dependiendo del estado on-off.
+// si esta funcion estuviese dentro de la clase, necesito invocarla con this.
+const texto = (t) => {
+    if (t === true) {
+        return <span className='on'>ON</span>
+    }
+    else {
+        return <span className='off'>OFF</span>
+    }
+}
 
+// inicializo clase House
 class House extends React.Component {
     state = {
-        kitchen: true,
-        bathroom: false,
-        livingRoom: true,
-        bedroom: false
+        piezas : {
+            kitchen: true,
+            bathroom: false,
+            livingRoom: true,
+            bedroom: false
+        }
     }
 
-    handleChanges = (event) => {
-        console.log('hola',event)
-        this.setState((state, props) => {
-            return{
-                kitchen: !this.state.kitchen,
-                bathroom: !this.state.bathroom,
-                livingRoom: !this.state.livingRoom
+    // esta funcion realiza los cambios de estado al prender o apagar la luz, si esta en true pasa a falso y viceversa
+    // utilizo el prev State para evitar que si pulso un boton cambie las luces del resto de las piezas
+    handleChanges = pieza => {
+        console.log(pieza)
+        this.setState(prevState => ({
+            piezas: { 
+                // pieza: prevState,
+                ...prevState,
+                [pieza] : !prevState.piezas[pieza]
             }
-        })
+        }))
         console.log(this.state)
     }
 
+    // esta funcion apaga las luces de toda la pieza, toma el estado que tenga y lo cambia a falso
+    turnOffAllLights = () => {
+        this.setState({
+            piezas: {
+            kitchen: false,
+            bathroom: false
+            }
+        })
+    }
+ 
+    // lo que se ve por pantalla
     render(){
+        
         return(
             <div>
-            <p>la luz de la cocina esta { this.state.kitchen == true ? 'prendida' : 'apagada'}</p>
-            <Lights onChange={this.handleChanges}/>
+            <p>La luz de la cocina esta {texto(this.state.piezas.kitchen)}</p>
+            <button onClick={() => this.handleChanges('kitchen')}>
+                Kitchen
+            </button>
+            <p> La luz del bano esta {texto(this.state.piezas.bathroom)}</p>
+            <button onClick={() => this.handleChanges('bathroom')}>
+                Bathroom
+            </button>
+            <p>Quieres apagar todas las luces?</p>
+            <button onDoubleClick ={this.turnOffAllLights}>Turn off the lights </button>
             </div>
         )
     }
 }
 
-function Lights({onChange}){
-    return(
-        <button onClick={onChange}>kitchen</button>
-    )
-}
+
 
 
 ReactDOM.render(
