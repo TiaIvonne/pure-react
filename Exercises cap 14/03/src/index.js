af84382a6d1b9556f0e249ca910d45ca1066642d
Ivonne M <ivonne.wftm@gmail.com>
2020-09-29 18:59:53 +0200

    29-09

--- Exercises cap 14/03/src/index.js
+++ Exercises cap 14/03/src/index.js
@@ -1,13 +1,16 @@
-import React, {useState} from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
 
+
+// tengo un panel de control base que es el que mostrara en pantalla los botones y el estilo
 const Control = ({
     value,
     children,
     onIncrease,
     onDecrease
 }) => (
+    // aqui esta el panel de los botones y le entrego una variable
         <div className="control">
             <button onClick={onDecrease}>&ndash;</button>
             <div>
@@ -18,15 +21,72 @@ const Control = ({
         </div>
     );
 
-const AudioControls = () => {
-    return (
-        <div className= 'audio-controls'>
-            <Control/>
-        </div>
-        )
-}
+const AudioControlsWithOneObject = () => {
+    const [{ volume, bass, mid, treble }, setValues] = useState({
+        volume: 53,
+        bass: 17,
+        mid: 51,
+        treble: 32
+    });
+
+    const increase = key => () => {
+        setValues(values => ({
+            ...values,
+            [key]: values[key] + 1
+        }));
+        console.log(key)
+    }
+
+
+    const decrease = key => () => {
+        setValues(values => ({
+            ...values,
+            [key]: values[key] - 1
+        }));
+    }
+
+    // retorno la plantilla base de control y le agrego las funciones que suman o restan numeros 
+    return <div className="audio-controls">
+        <Control
+            value={volume}
+            // a la variable onIncrease declarada en control le entrego la funcion increase 
+            onIncrease={increase('volume')}
+            onDecrease={decrease('volume')}
+        >
+            Volume
+      </Control>
+
+        <Control
+            value={treble}
+            onIncrease={increase('treble')}
+            onDecrease={decrease('treble')}
+        >
+            Treble
+      </Control>
+
+        <Control
+            value={mid}
+            onIncrease={increase('mid')}
+            onDecrease={decrease('mid')}
+        >
+            Mid
+      </Control>
+
+        <Control
+            value={bass}
+            onIncrease={increase('bass')}
+            onDecrease={decrease('bass')}
+        >
+            Bass
+      </Control>
+    </div>
+};
 
 ReactDOM.render(
-    <AudioControls/>,
-    document.getElementById('root')
-)
+    <>
+
+        <h1>With A Single Object</h1>
+        <AudioControlsWithOneObject />
+    </>,
+    document.querySelector('#root')
+);
